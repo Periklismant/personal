e28923dd274375f83a34ca93907e83bb4c7a4202
Periklismant <pmantenoglou@iit.demokritos.gr>
2024-08-23 17:59:31 +0300

    Website changes for: about, education and experience$

--- push.sh
+++ push.sh
@@ -5,12 +5,14 @@ COMMIT_MESSAGE=$1
 cd public
 git add .
 git commit -m "${COMMIT_MESSAGE}"
+git push
 
 cd ../themes/hugo-profile
 git add .
 git commit -m "Theme changes for: ${COMMIT_MESSAGE}" 
+git push
 
 cd ../..
 git add .
 git commit -m "Website changes for: ${COMMIT_MESSAGE}$"
-git push --recurse-submodules=on-demand
+git push
